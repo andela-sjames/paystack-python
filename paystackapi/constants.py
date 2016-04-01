3e563d49449816dce66c5a66c29ab654eed508f6
andela-sjames <samuel.james@andela.com>
2016-04-01 18:39:53 +0100

    change staticmethod to classmethod.

--- paystackapi/constants.py
+++ paystackapi/constants.py
@@ -3,11 +3,3 @@
 PAYSTACK_SECRET_KEY = 'sk_test_0a246ef179dc841f42d20959bebdd790f69605d8'
 HEADERS = {'Authorization': 'Bearer ' + PAYSTACK_SECRET_KEY}
 api_url = 'https://api.paystack.co/'
-
-
-class Paystack(object):
-    """Classs used to instantiate paystack."""
-
-    def __init__(self, PAYSTACK_SECRET_KEY=None):
-        self.paystack_secret = PAYSTACK_SECRET_KEY
-        pass

395617afca4d242de12e2a75a3ae7d2a258f75a7
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-04-02 17:18:27 +0100

    use template string

--- paystackapi/constants.py
+++ paystackapi/constants.py
@@ -2,5 +2,5 @@
 
 
 PAYSTACK_SECRET_KEY = 'sk_test_0a246ef179dc841f42d20959bebdd790f69605d8'
-HEADERS = {'Authorization': 'Bearer ' + PAYSTACK_SECRET_KEY}
+HEADERS = {'Authorization': 'Bearer {}'}
 API_URL = 'https://api.paystack.co/'

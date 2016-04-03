0e44dc51200218ae7ba5ff34d47e5a7a5ab42d37
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-04-03 13:43:29 +0100

    move constants

--- paystackapi/constants.py
+++
@@ -1,6 +0,0 @@
-"""Script used to define constants used across codebase."""
-
-
-PAYSTACK_SECRET_KEY = 'sk_test_0a246ef179dc841f42d20959bebdd790f69605d8'
-HEADERS = {'Authorization': 'Bearer {}'}
-API_URL = 'https://api.paystack.co/'

69a339c792e2545cbd12c126a5b0865e4cf1e7e5
andela-sjames <samuel.james@andela.com>
2019-08-27 22:43:40 +0000

    Add test cases for product.

--- paystackapi/tests/test_product.py
+++ paystackapi/tests/test_product.py
@@ -4,8 +4,66 @@ from paystackapi.tests.base_test_case import BaseTestCase
 from paystackapi.product import Product
 
 
-# class TestProduct(BaseTestCase):
+class TestProduct(BaseTestCase):
 
-# 	@httpretty.activate
-# 	def test_valid_create(self):
-#         pass
+    @httpretty.activate
+    def test_product_create(self):
+        """Method defined to test product creation."""
+        httpretty.register_uri(
+            httpretty.POST,
+            self.endpoint_url("/product"),
+            content_type='text/json',
+            body='{"status": true,  "message": "Product successfully created"}',
+            status=201,
+        )
+
+        response = Product.create(
+            name="Product pypaystack test", description="my test description",
+            price=500000, currency="NGN"
+        )
+        self.assertTrue(response['status'])
+
+    @httpretty.activate
+    def test_product_list(self):
+        """Function defined to test Product list method."""
+        httpretty.register_uri(
+            httpretty.GET,
+            self.endpoint_url("/product"),
+            content_type='text/json',
+            body='{"status": true, "message": "Products retrieved", "data":[{}], "meta":{}}',
+            status=201,
+        )
+
+        response = Product.list()
+        self.assertEqual(response['status'], True)
+
+    @httpretty.activate
+    def test_product_fetch(self):
+        """Function defined to test Product list method."""
+        httpretty.register_uri(
+            httpretty.GET,
+            self.endpoint_url("/product/5499"),
+            content_type='text/json',
+            body='{"status": true, "message": "Products retrieved", "data":[{}]}',
+            status=201,
+        )
+
+        response = Product.fetch(5499)
+        self.assertEqual(response['status'], True)
+
+    @httpretty.activate
+    def test_product_fetch(self):
+        """Function defined to test Product list method."""
+        httpretty.register_uri(
+            httpretty.PUT,
+            self.endpoint_url("/product/5499"),
+            content_type='text/json',
+            body='{"status": true, "message": "Products retrieved", "data":[{}]}',
+            status=201,
+        )
+
+        response = Product.update(product_id=5499, name="Product pypaystack test",
+            description="my test description", price=500000000,
+            currency="USD"
+        )
+        self.assertEqual(response['status'], True)

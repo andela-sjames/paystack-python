33c491b8ba07e342f9f550da7e1e1802860f53fd
andela-sjames <samuel.james@andela.com>
2019-08-28 17:56:46 +0000

    Add list subaccount

--- paystackapi/subaccount.py
+++ paystackapi/subaccount.py
@@ -22,3 +22,18 @@ class SubAccount(PayStackBase):
             Json data from paystack API.
         """
         return cls().requests.post('subaccount', data=kwargs,)
+
+    @classmethod
+	def list(cls, **kwargs):
+		"""
+		List subaccounts
+		
+		Args:
+			perPage: records you want to retrieve per page (Integer)
+			page: what page you want to retrieve (Integer)
+		
+		Returns:
+		JSON data from paystack's API.
+		"""
+		
+		return cls().requests.get('subaccount', data=kwargs)

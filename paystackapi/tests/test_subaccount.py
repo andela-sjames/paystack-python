0901477d231091e72b4e47e0f5a59a49cb31414d
andela-sjames <samuel.james@andela.com>
2019-08-28 23:50:57 +0000

    Add test subaccount test creation

--- paystackapi/tests/test_subaccount.py
+++ paystackapi/tests/test_subaccount.py
@@ -9,3 +9,18 @@ class TestSubAccount(BaseTestCase):
     @httpretty.activate
     def test_subaccount_create(self):
         pass
+
+        """Method defined to test subaccount creation."""
+        httpretty.register_uri(
+            httpretty.POST,
+            self.endpoint_url("/subaccount"),
+            content_type='text/json',
+            body='{"status": true, "message": "Subaccount created"}',
+            status=201,
+        )
+
+        response = SubAccount.create(
+            business_name="Test Biz 123", settlement_bank="Access Bank",
+            account_number="xxxxxxxxx", percentage_charge="6.9"
+        )
+        self.assertTrue(response['status'])

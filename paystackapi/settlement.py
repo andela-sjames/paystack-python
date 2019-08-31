02d4315442bd5e7589822d4763d53e61ee4538ca
andela-sjames <samuel.james@andela.com>
2019-08-31 21:09:38 +0000

    Add settlements

---
+++ paystackapi/settlement.py
@@ -0,0 +1,25 @@
+"""Script used to define the paystack settlement class."""
+
+from paystackapi.base import PayStackBase
+
+
+class Settlement(PayStackBase):
+    """docstring for settlement."""
+
+    @classmethod
+    def fetch(cls, start_date, end_date, subaccount):
+        """
+        Function defined to fetch settlement.
+
+        Args:
+            start_date: Lower bound of date range. 
+                  Leave undefined to export settlement from day one.
+            end_date: Upper bound of date range. 
+                Leave undefined to export settlements till date.
+            subaccount: code to export only settlements for that subaccount. 
+                        Set to none to export only transactions for the account.
+
+        Returns:
+            Json data from paystack API.
+        """
+        return cls().requests.get(f"subaccount?from={start_date}&to={end_date}&subaccount={subaccount}")

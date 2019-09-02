ea3576e16b0a8278cd9d35715c8881e9d136eec8
andela-sjames <samuel.james@andela.com>
2019-09-02 14:06:39 +0000

    Add control panel test for update

--- paystackapi/tests/test_cpanel.py
+++ paystackapi/tests/test_cpanel.py
@@ -19,3 +19,17 @@ class TestPage(BaseTestCase):
 
         response = ControlPanel.fetch_payment_session_timeout()
         self.assertTrue(response['status'])
+
+    @httpretty.activate
+    def test_fetch_payment_session_timeout(self):
+        """Method defined to test update payment session timeout."""
+        httpretty.register_uri(
+            httpretty.PUT,
+            self.endpoint_url("/integration/payment_session_timeout"),
+            content_type='text/json',
+            body='{"status": true, "message": "Payment session timeout updated"}',
+            status=201,
+        )
+
+        response = ControlPanel.update_payment_session_timeout(timeout=30)
+        self.assertTrue(response['status'])

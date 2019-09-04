151e8fc71e5ef2e31db13730bff57bc8fd915c30
andela-sjames <samuel.james@andela.com>
2019-09-04 14:09:46 +0000

    Add test case for list invoice

---
+++ paystackapi/tests/test_invoice.py
@@ -0,0 +1,45 @@
+import httpretty
+
+from paystackapi.tests.base_test_case import BaseTestCase
+from paystackapi.invoice import Invoice
+
+
+class TestInvoice(BaseTestCase):
+
+    @httpretty.activate
+    def test_create_invoice(self):
+        """Method defined to test create Invoice."""
+        httpretty.register_uri(
+            httpretty.POST,
+            self.endpoint_url("/paymentrequest"),
+            content_type='text/json',
+            body='{"status": true, "message": "Invoice created"}',
+            status=201,
+        )
+
+        response = Invoice.create(
+            customer="CUS_je02lbimlqixzax",
+            amount=42000,
+            due_date="2019-05-08T00:00:00.000Z"
+        )
+        self.assertTrue(response['status'])
+
+    @httpretty.activate
+    def test_list_invoice(self):
+        """Method defined to test list Invoice."""
+        httpretty.register_uri(
+            httpretty.GET,
+            self.endpoint_url("/paymentrequest"),
+            content_type='text/json',
+            body='{"status": true, "message": "Invoice retrieved"}',
+            status=201,
+        )
+
+        response = Invoice.list(
+            customer="CUS_je02lbimlqixzax",
+            status="pending",
+            currency="NGN",
+            paid="false",
+            include_archive="true"
+        )
+        self.assertTrue(response['status'])

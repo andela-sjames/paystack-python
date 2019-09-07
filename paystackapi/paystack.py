58fccc6be074e384435dd9ab618e0597d63b881c
andela-sjames <samuel.james@andela.com>
2019-09-07 22:14:22 +0000

    Update readme

--- paystackapi/paystack.py
+++ paystackapi/paystack.py
@@ -11,6 +11,7 @@ from paystackapi.settlement import Settlement
 from paystackapi.subaccount import SubAccount
 from paystackapi.subscription import Subscription
 from paystackapi.transaction import Transaction
+from paystackapi.trecipient import TransferRecipient
 from paystackapi.verification import Verification
 
 from paystackapi.base import PayStackBase
@@ -34,5 +35,6 @@ class Paystack(PayStackBase):
         self.subaccount = SubAccount
         self.subscription = Subscription
         self.transaction = Transaction
+        self.transferRecipient = TransferRecipient
         self.verification = Verification
 

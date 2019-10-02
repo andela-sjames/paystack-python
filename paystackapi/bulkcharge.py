57f83f27fd4eb49362edee934970ca75b3806e30
andela-sjames <samuel.james@andela.com>
2019-10-03 00:13:08 +0100

    fetch_charges_batch

--- paystackapi/bulkcharge.py
+++ paystackapi/bulkcharge.py
@@ -24,14 +24,13 @@ class BulkCharge(PayStackBase):
         """
         return cls().requests.post('bulkcharge', data=kwargs)
 
-
     @classmethod
     def list(cls, **kwargs):
         """
         List Bulk Charge Batches created by the integration.
 
         Args:
-            perPage: Number of transfers listed per page for pagination
+            perPage: Number of transfer listed per page for pagination
             page: number of pages listed by pagination.
 
         Returns:
@@ -41,25 +40,29 @@ class BulkCharge(PayStackBase):
         return cls().requests.get('bulkcharge', qs=kwargs,)
 
     @classmethod
-    def fetch_bulk_charge_batch(cls, **kwargs):
-        """ 
-        This endpoint retrieves a specific batch code. 
-        It also returns useful information on its progress by way of the total_charges and pending_charges attributes.
+    def fetch_bulk_batch(cls, id_or_code):
+        """ This endpoint retrieves a specific batch code.
 
+        It also returns useful information on its progress
+        by way of the total_charges and pending_charges attributes.
+
+        Args:
+            id_or_code: An ID or code for the transfer whose 
+                        details you want to retrieve.
+        
         Returns:
             Json data from paystack API
         """
-        return cls().requests.get("bulkcharge", data=kwargs)
-
-
+        return cls().requests.get(f"bulkcharge/{id_or_code}")
 
     @classmethod
-    def fetch_charges(cls, **kwargs):
+    def fetch_charges_batch(cls, **kwargs):
         """
         Fetch the charges associated with a specified batch code.
 
         Args:
             id_or_code: An ID or code for the batch whose charges you want to retrieve.
+            status: pending, success or failed
             perPage: Number of transfers listed per page for pagination
             page: number of pages listed by pagination.
 
@@ -67,9 +70,8 @@ class BulkCharge(PayStackBase):
             Json data from paystack API.
         """
 
-        return cls().requests.get(f"bulkcharge/{id_or_code}/charges", data=kwargs)
+        return cls().requests.get(f"bulkcharge/{id_or_code}/charges", qs=kwargs)
 
-    
     @classmethod
     def pause_bulk_charge(cls, **kwargs):
         """
@@ -83,7 +85,6 @@ class BulkCharge(PayStackBase):
         """
         return cls().requests.get(f"bulkcharge/pause/{batch_code}", data=kwargs)
 
-    
     @classmethod
     def resume_bulk_charge(cls, **kwargs):
         """

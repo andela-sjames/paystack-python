56237b99d33379ae0d45577cac79e9edb0e307f7
samuelvarejames <samuelvarejames@gmail.com>
2024-08-08 01:10:25 +0200

    Update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='paystackapi',
-    version='2.1.2',
+    version='2.1.3',
     description='A python library to consume Paystack API',
     long_description=long_description,
     long_description_content_type='text/markdown',
